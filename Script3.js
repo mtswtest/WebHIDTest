36e6c553fca125eb43d3456683b929e3995a1d45
mtswtest <mtswtest@gmail.com>
2020-04-23 09:27:33 -0700

    Update Script3.js

--- Script3.js
+++ Script3.js
@@ -2,8 +2,9 @@ document.addEventListener('DOMContentLoaded', event => {
     let button = document.getElementById('ShowHIDDevices')
 
     button.addEventListener('click', async () => {
-
-		let device = await navigator.hid.requestDevice({filters:[]});
+		let deviceFilter = { vendorId: 0x0801, productId: 0x2020 };
+		let requestParams  = { filters: [deviceFilter] };
+		let device = await navigator.hid.requestDevice(requestParams);
 
         console.log('done');
     })
