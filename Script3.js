8415d6339cc89fee289ef5754270781b0cefd9a4
mtswtest <mtswtest@gmail.com>
2020-04-23 10:54:11 -0700

    Update Script3.js

--- Script3.js
+++ Script3.js
@@ -25,7 +25,7 @@ document.addEventListener('DOMContentLoaded', event => {
 		device.open().then(() => {
 			console.log('Opened HID device');
 			device.addEventListener('inputreport', handleInputReport);
-			device.sendReport(0x00, commandReport).then(() => {
+			device.sendReport(0x01, commandReport).then(() => {
 				console.log('Sent command');
 			});
         });
