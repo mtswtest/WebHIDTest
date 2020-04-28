1b8a17a8179750c3fbaad65973b8f9d59b340988
mtswtest <mtswtest@gmail.com>
2020-04-27 17:37:11 -0700

    Update mmxusbservice.js

--- mmxusbservice.js
+++ mmxusbservice.js
@@ -29,7 +29,7 @@ var mmxusbservice = (function () {
 		return eventTarget.removeEventListener(type, listener, useCapture);
 	};
 
-	mmxusbservice.prototype.openDevice = async function () {
+	async mmxusbservice.prototype.openDevice = async function () {
 		let deviceFilter = { vendorId: 0x0801, productId: 0x2020 };
 		let requestParams  = { filters: [deviceFilter] };
 
