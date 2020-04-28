7545edc0dc708595be6f5809a7a0cb2a4331254c
mtswtest <mtswtest@gmail.com>
2020-04-27 18:08:51 -0700

    Update mmxusbservice.js

--- mmxusbservice.js
+++ mmxusbservice.js
@@ -226,19 +226,17 @@ var mmxusbservice = (function () {
 
 mmxusbservice["__class"] = "mmxusbservice";
 
-function hexStringToByte(uint8arr) {
-  if (!uint8arr) {
-	return '';
-  }
-  
-  var hexStr = '';
-  for (var i = 0; i < uint8arr.length; i++) {
-	var hex = (uint8arr[i] & 0xff).toString(16);
-	hex = (hex.length === 1) ? '0' + hex : hex;
-	hexStr += hex;
-  }
-  
-  return hexStr.toUpperCase();
+function hexStringToByte(str) {
+	  if (!str) {
+		return new Uint8Array();
+	  }
+	  
+	  var a = [];
+	  for (var i = 0, len = str.length; i < len; i+=2) {
+		a.push(parseInt(str.substr(i,2),16));
+	  }
+	  
+	  return new Uint8Array(a);
 }
 	
 async function testDevice() {
